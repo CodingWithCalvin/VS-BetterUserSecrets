a5c650d4085abcbb91be0599963fcc451a70bfab
Calvin A. Allen <calvin.allen@outlook.com>
2019-06-07 23:19:58 -0400

    Adding command to load a previous secrets file, or create a new one and update the project file accordingly

--- src/BetterUserSecrets/BetterUserSecretsPackage.cs
+++ src/BetterUserSecrets/BetterUserSecretsPackage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+using BetterUserSecrets.Commands;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
@@ -24,31 +25,18 @@ namespace BetterUserSecrets
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.1.12", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(BetterUserSecretsPackage.PackageGuidString)]
+    [Guid(PackageGuids.guidPackageString)]
     public sealed class BetterUserSecretsPackage : AsyncPackage
     {
-        /// <summary>
-        /// BetterUserSecretsPackage GUID string.
-        /// </summary>
-        public const string PackageGuidString = "c21f6a3b-6907-4073-bef4-f0d4a59069ac";
-
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-        }
 
-        #endregion
+            BetterUserSecretsCommand.Initialize(this);
+        }
     }
 }
---
+++ src/BetterUserSecrets/Commands/BetterUserSecretsCommand.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+
+namespace BetterUserSecrets.Commands
+{
+    internal sealed class BetterUserSecretsCommand
+    {
+        private readonly Package _package;
+        
+        public static BetterUserSecretsCommand Instance { get; private set; }
+        private IServiceProvider ServiceProvider => this._package;
+
+        private BetterUserSecretsCommand(Package package)
+        {
+            this._package = package;
+
+            var commandService = (OleMenuCommandService)ServiceProvider.GetService(typeof(IMenuCommandService));
+
+            if (commandService == null)
+            {
+                return;
+            }
+
+            var menuCommandId = new CommandID(PackageGuids.guidBUSCmdSet, PackageIds.BUSId);
+            var menuItem = new MenuCommand(GetOrCreateSecretsFile, menuCommandId);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new BetterUserSecretsCommand(package);
+        }
+
+        private void GetOrCreateSecretsFile(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                var service = (DTE2)this.ServiceProvider.GetService(typeof(DTE));
+                Assumes.Present(service);
+
+                var userSecretsId = GetOrUpdateProject(service);
+
+                var secretsFile = GetOrCreateSecretsFile(userSecretsId, service);
+
+                service.ItemOperations.OpenFile(secretsFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static string GetOrUpdateProject(DTE2 service)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var selectedItem = (Array)service.ToolWindows.SolutionExplorer.SelectedItems;
+            var uiItem = selectedItem.GetValue(0) as UIHierarchyItem;
+
+            if (!(uiItem?.Object is Project project))
+            {
+                throw new ArgumentException("Selected Item is not Project File");
+            }
+
+            var projectXml = new XmlDocument();
+            projectXml.Load(project.FullName);
+
+            var frameworkNode = projectXml.SelectSingleNode("/Project/PropertyGroup/TargetFramework");
+            var userSecretsId = projectXml.SelectSingleNode("/Project/PropertyGroup/UserSecretsId/text()")?.Value;
+            var propGroupNode = frameworkNode?.ParentNode;
+
+            //couldn't find the TargetFramework node which we want to put the UserSecretsId in.
+            if (propGroupNode == null)
+            {
+                throw new ArgumentException("Unable to locate the TargetFramework element and/or its parent node.");
+            }
+
+            if (string.IsNullOrWhiteSpace(userSecretsId))
+            {
+                userSecretsId = CreateUserSecretsId(projectXml, propGroupNode, project);
+            }
+
+            return userSecretsId;
+        }
+
+        private static string GetOrCreateSecretsFile(string userSecretsId, DTE2 service)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var dir = Path.GetFullPath($"{appData}\\Microsoft\\UserSecrets\\{userSecretsId}");
+
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            var secretsFile = Path.Combine(dir, "secrets.json");
+
+            if (!File.Exists(secretsFile))
+            {
+                File.WriteAllText(secretsFile, "{\r\n\r\n}");
+            }
+
+            return secretsFile;
+        }
+
+        private static string CreateUserSecretsId(XmlDocument projectXml, XmlNode propGroupNode, Project project)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var userSecretsId = Guid.NewGuid().ToString();
+
+            var userSecretsElement = projectXml.CreateElement("UserSecretsId");
+            userSecretsElement.InnerText = userSecretsId;
+            propGroupNode.AppendChild(userSecretsElement);
+
+            var settings = new XmlWriterSettings()
+            {
+                Indent = true,
+                IndentChars = "  ",
+                NewLineHandling = NewLineHandling.None,
+                NewLineChars = Environment.NewLine,
+                OmitXmlDeclaration = true
+            };
+
+            using (var writer = XmlWriter.Create(project.FullName, settings))
+            {
+                projectXml.Save(writer);
+            }
+
+            return userSecretsId;
+        }
+    }
+}
